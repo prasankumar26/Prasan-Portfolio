afe4044d3cb6af7262f42fab0d6248221b7b87f3
mumbainames <mumbainames@gmail.com>
2022-01-19 23:08:38 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link,  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar'
 import Home from './components/Home';
 import About from './components/About';
